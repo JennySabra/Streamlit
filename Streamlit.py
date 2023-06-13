907c705b85c49ace02159bb8c772d6c06d6c96cf
JennySabra <114403465+JennySabra@users.noreply.github.com>
2023-06-13 16:02:27 +0300

    Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.graph_objects as go
 from matplotlib.gridspec import GridSpec
